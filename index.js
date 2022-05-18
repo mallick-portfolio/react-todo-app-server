95bec700666c46c2de99ba0a0fdb299d9d821093
mallick-portfolio <tamal@gmail.com>
2022-05-19 02:58:40 +0600

    updated port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
